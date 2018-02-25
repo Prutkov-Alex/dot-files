e4d34d7e3eadb86f7a04eb9b59a69a34b08437df
Prutkov-Alex <prutkov.alex@gmail.com>
2018-02-25 10:26:39 +0200

    Fixed symlinks in deploy

--- deploy.sh
+++ deploy.sh
@@ -11,27 +11,27 @@ bash ./installs.sh
 echo "Successfully installed packages for $SUDO_USER"
 
 ROOTDIR=$(pwd)
-
 BackupAndSymlink () {
 	if [ -f $1 -o -d $1 ]; then
 		echo "Backing up $1..."
-		mv $1 ~/.conf.bak/
+		mv $1 $HOME/.conf.bak/
 	else
 		echo "Path $1 does not exist"
 	fi
 
 	echo "Generating symlink $1 -> $2"
-	ln -s $2 $1
+	ln -s "$2" "$1"
 }
 
-if ! [ -d ~/.conf.bak ]; then
-	echo "Creating ~/.conf.bak for backups"
+if ! [ -d $HOME/.conf.bak ]; then
+	echo "Creating $HOME/.conf.bak for backups"
+	mkdir $HOME/.conf.bak
 fi
 
-BackupAndSymlink "~/.vim" "$ROOTDIR/.vim"
-BackupAndSymlink "~/.vimrc" "$ROOTDIR/.vimrc"
-BackupAndSymlink "~/.oh-my-zsh" "$ROOTDIR/.oh-my-zsh"
-BackupAndSymlink "~/.zshrc" "$ROOTDIR/.zshrc"
-BackupAndSymlink "~/.tmux.conf" "$ROOTDIR/.tmux.conf"
+BackupAndSymlink "$HOME/.vim" "$ROOTDIR/.vim"
+BackupAndSymlink "$HOME/.vimrc" "$ROOTDIR/.vimrc"
+BackupAndSymlink "$HOME/.oh-my-zsh" "$ROOTDIR/.oh-my-zsh"
+BackupAndSymlink "$HOME/.zshrc" "$ROOTDIR/.zshrc"
+BackupAndSymlink "$HOME/.tmux.conf" "$ROOTDIR/.tmux.conf"
 
 exit 0
