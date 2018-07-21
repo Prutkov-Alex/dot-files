67043a27afc8b11ad8e308a42f2714837db764a8
user <user@mail.com>
2018-07-21 20:38:06 +0300

    Adding profile + changing theme

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-
 set -e
+
+BackupAndSymlink () {
+	if [ -f $1 -o -d $1 ]; then
+		echo "Backing up $1..."
+		mv $1 $HOME/.conf.bak/
+	else
+		echo "Path $1 does not exist"
+	fi
+
+	echo "Generating symlink $1 -> $2"
+	ln -s "$2" "$1"
+}
+
+GenProfile () {
+	echo "Adding solarized-dark theme to terminal profiles..."
+	UUID=$(uuidgen)
+	echo "Generated UUID: $UUID"
+	cat profile.dconf | dconf load "/org/gnome/terminal/legacy/profiles:/:$UUID/"
+	dconf write "/org/gnome/terminal/legacy/profiles:/default" "'$UUID'"
+	#TODO: add to list of visible profiles
+}
+
 if ! [ $(id -u) != 0 ]; then
 	echo "This script must NOT have root privileges."
 	echo "I will ask for root password if needed"
@@ -14,18 +35,6 @@ bash ./installs.sh
 echo "Successfully installed packages for $SUDO_USER"
 
 ROOTDIR=$(pwd)
-BackupAndSymlink () {
-	if [ -f $1 -o -d $1 ]; then
-		echo "Backing up $1..."
-		mv $1 $HOME/.conf.bak/
-	else
-		echo "Path $1 does not exist"
-	fi
-
-	echo "Generating symlink $1 -> $2"
-	ln -s "$2" "$1"
-}
-
 if ! [ -d $HOME/.conf.bak ]; then
 	echo "Creating $HOME/.conf.bak for backups"
 	mkdir $HOME/.conf.bak
@@ -37,4 +46,12 @@ BackupAndSymlink "$HOME/.oh-my-zsh" "$ROOTDIR/.oh-my-zsh"
 BackupAndSymlink "$HOME/.zshrc" "$ROOTDIR/.zshrc"
 BackupAndSymlink "$HOME/.tmux.conf" "$ROOTDIR/.tmux.conf"
 
+GenProfile
+
+echo "Change default shell to zsh..."
+chsh -s /bin/zsh
+
+echo "Logging out to apply changes"
+gnome-session-quit --logout
+exit 0
 exit 0
